307197b6145d574e3d52a3be835b90e1353f7d81
Deepyash <158832695+yashLGN@users.noreply.github.com>
2026-06-20 23:32:19 +0530

    v0.75 Losses functions intial

--- losses.py
+++ losses.py
@@ -1,1 +1,9 @@
-# WIP
+from matrix import Matrix
+
+class Losses:
+    @staticmethod
+    def mse(predicted, actual):
+        if predicted.shape != actual.shape:
+            raise ValueError("Predicted and actual matrices must have the same shape.")
+        if not isinstance(predicted, Matrix) or not isinstance(actual, Matrix):
+            raise TypeError("Predicted and actual must be Matrix instances.")
